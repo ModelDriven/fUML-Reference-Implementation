1508bf48140f8cf68f72a80ffcae849af3e6eaf3
Ed Seidewitz <ed-s@modeldriven.com>
2017-01-01 12:34:45 -0500

    Issue #28 - Corrected problem w/ starting multiple classifier behaviors.

--- org.modeldriven.fuml/src/main/java/fUML/Semantics/CommonBehaviors/Communications/ObjectActivation_Behavior.java
+++ org.modeldriven.fuml/src/main/java/fUML/Semantics/CommonBehaviors/Communications/ObjectActivation_Behavior.java
@@ -3,7 +3,7 @@
  * Initial version copyright 2008 Lockheed Martin Corporation, except  
  * as stated in the file entitled Licensing-Information. 
  * 
- * All modifications copyright 2009-2012 Data Access Technologies, Inc.
+ * All modifications copyright 2009-2016 Data Access Technologies, Inc.
  *
  * Licensed under the Academic Free License version 3.0 
  * (http://www.opensource.org/licenses/afl-3.0.php), except as stated 
@@ -26,20 +26,10 @@ public class ObjectActivation_Behavior {
 	public void _startObjectBehavior() {
 		// *** This should start the EventDispatchLoop ***
 
-		/*
-		while (this.signalCount > 0) {
-			this.self.dispatchNextEvent();
-			signalCount = signalCount - 1;
-		}
-		*/
-		
 		if (this.execution == null) {
 			this.execution = new ObjectActivationExecution(this);
 		}
 		
-		if (this.signalCount > 0) {
-			ExecutionQueue.enqueue(execution);
-		}
 	} // _startObjectBehavior
 
 	public void _send(
@@ -49,14 +39,20 @@ public class ObjectActivation_Behavior {
 		
 		this.signalCount = this.signalCount + 1;
 		if (this.signalCount == 1) {
-			this._startObjectBehavior();
+			this.enqueue();
 		}
 	} // _send
 	
 	public void dispatchNextEvent() {
 		this.self.dispatchNextEvent();
 		signalCount = signalCount - 1;
-		this._startObjectBehavior();
+		if (this.signalCount > 0) {
+			this.enqueue();
+		}
+	}
+	
+	private void enqueue() {
+		ExecutionQueue.enqueue(this.execution);
 	}
 
 }
