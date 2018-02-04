520fc6fd70b2d1fe93ffbd8c5effcbc07ae0a8f0
Ed Seidewitz <ed-s@modeldriven.com>
2018-02-04 16:29:04 -0500

    Issue FUML14-9 - Put all ExecutionFactory functionality in base class.

--- org.modeldriven.fuml/src/main/java/fuml/semantics/loci/ExecutionFactory.java
+++ org.modeldriven.fuml/src/main/java/fuml/semantics/loci/ExecutionFactory.java
@@ -3,7 +3,7 @@
  * Initial version copyright 2008 Lockheed Martin Corporation, except  
  * as stated in the file entitled Licensing-Information. 
  * 
- * All modifications copyright 2009-2017 Data Access Technologies, Inc.
+ * All modifications copyright 2009-2018 Data Access Technologies, Inc.
  *
  * Licensed under the Academic Free License version 3.0 
  * (http://www.opensource.org/licenses/afl-3.0.php), except as stated 
@@ -13,13 +13,115 @@
 package fuml.semantics.loci;
 
 import fuml.Debug;
+import fuml.semantics.actions.AcceptCallActionActivation;
+import fuml.semantics.actions.AcceptEventActionActivation;
+import fuml.semantics.actions.AddStructuralFeatureValueActionActivation;
+import fuml.semantics.actions.CallBehaviorActionActivation;
+import fuml.semantics.actions.CallOperationActionActivation;
+import fuml.semantics.actions.ClearAssociationActionActivation;
+import fuml.semantics.actions.ClearStructuralFeatureActionActivation;
+import fuml.semantics.actions.ConditionalNodeActivation;
+import fuml.semantics.actions.CreateLinkActionActivation;
+import fuml.semantics.actions.CreateObjectActionActivation;
+import fuml.semantics.actions.DestroyLinkActionActivation;
+import fuml.semantics.actions.DestroyObjectActionActivation;
+import fuml.semantics.actions.ExpansionNodeActivation;
+import fuml.semantics.actions.ExpansionRegionActivation;
+import fuml.semantics.actions.InputPinActivation;
+import fuml.semantics.actions.LoopNodeActivation;
+import fuml.semantics.actions.OutputPinActivation;
+import fuml.semantics.actions.ReadExtentActionActivation;
+import fuml.semantics.actions.ReadIsClassifiedObjectActionActivation;
+import fuml.semantics.actions.ReadLinkActionActivation;
+import fuml.semantics.actions.ReadSelfActionActivation;
+import fuml.semantics.actions.ReadStructuralFeatureActionActivation;
+import fuml.semantics.actions.ReclassifyObjectActionActivation;
+import fuml.semantics.actions.ReduceActionActivation;
+import fuml.semantics.actions.RemoveStructuralFeatureValueActionActivation;
+import fuml.semantics.actions.ReplyActionActivation;
+import fuml.semantics.actions.SendSignalActionActivation;
+import fuml.semantics.actions.StartClassifierBehaviorActionActivation;
+import fuml.semantics.actions.StartObjectBehaviorActionActivation;
+import fuml.semantics.actions.StructuredActivityNodeActivation;
+import fuml.semantics.actions.TestIdentityActionActivation;
+import fuml.semantics.actions.ValueSpecificationActionActivation;
+import fuml.semantics.activities.ActivityExecution;
+import fuml.semantics.activities.ActivityFinalNodeActivation;
+import fuml.semantics.activities.ActivityParameterNodeActivation;
+import fuml.semantics.activities.CentralBufferNodeActivation;
+import fuml.semantics.activities.DataStoreNodeActivation;
+import fuml.semantics.activities.DecisionNodeActivation;
+import fuml.semantics.activities.FlowFinalNodeActivation;
+import fuml.semantics.activities.ForkNodeActivation;
+import fuml.semantics.activities.InitialNodeActivation;
+import fuml.semantics.activities.JoinNodeActivation;
+import fuml.semantics.activities.MergeNodeActivation;
+import fuml.semantics.commonbehavior.CallEventBehavior;
+import fuml.semantics.commonbehavior.CallEventExecution;
 import fuml.semantics.commonbehavior.Execution;
 import fuml.semantics.commonbehavior.OpaqueBehaviorExecution;
 import fuml.semantics.values.Evaluation;
+import fuml.semantics.values.InstanceValueEvaluation;
+import fuml.semantics.values.LiteralBooleanEvaluation;
+import fuml.semantics.values.LiteralIntegerEvaluation;
+import fuml.semantics.values.LiteralNullEvaluation;
+import fuml.semantics.values.LiteralRealEvaluation;
+import fuml.semantics.values.LiteralStringEvaluation;
+import fuml.semantics.values.LiteralUnlimitedNaturalEvaluation;
+import fuml.syntax.actions.AcceptCallAction;
+import fuml.syntax.actions.AcceptEventAction;
+import fuml.syntax.actions.AddStructuralFeatureValueAction;
+import fuml.syntax.actions.CallBehaviorAction;
+import fuml.syntax.actions.CallOperationAction;
+import fuml.syntax.actions.ClearAssociationAction;
+import fuml.syntax.actions.ClearStructuralFeatureAction;
+import fuml.syntax.actions.ConditionalNode;
+import fuml.syntax.actions.CreateLinkAction;
+import fuml.syntax.actions.CreateObjectAction;
+import fuml.syntax.actions.DestroyLinkAction;
+import fuml.syntax.actions.DestroyObjectAction;
+import fuml.syntax.actions.ExpansionNode;
+import fuml.syntax.actions.ExpansionRegion;
+import fuml.syntax.actions.InputPin;
+import fuml.syntax.actions.LoopNode;
+import fuml.syntax.actions.OutputPin;
+import fuml.syntax.actions.ReadExtentAction;
+import fuml.syntax.actions.ReadIsClassifiedObjectAction;
+import fuml.syntax.actions.ReadLinkAction;
+import fuml.syntax.actions.ReadSelfAction;
+import fuml.syntax.actions.ReadStructuralFeatureAction;
+import fuml.syntax.actions.ReclassifyObjectAction;
+import fuml.syntax.actions.ReduceAction;
+import fuml.syntax.actions.RemoveStructuralFeatureValueAction;
+import fuml.syntax.actions.ReplyAction;
+import fuml.syntax.actions.SendSignalAction;
+import fuml.syntax.actions.StartClassifierBehaviorAction;
+import fuml.syntax.actions.StartObjectBehaviorAction;
+import fuml.syntax.actions.StructuredActivityNode;
+import fuml.syntax.actions.TestIdentityAction;
+import fuml.syntax.actions.ValueSpecificationAction;
+import fuml.syntax.activities.Activity;
+import fuml.syntax.activities.ActivityFinalNode;
+import fuml.syntax.activities.ActivityParameterNode;
+import fuml.syntax.activities.CentralBufferNode;
+import fuml.syntax.activities.DataStoreNode;
+import fuml.syntax.activities.DecisionNode;
+import fuml.syntax.activities.FlowFinalNode;
+import fuml.syntax.activities.ForkNode;
+import fuml.syntax.activities.InitialNode;
+import fuml.syntax.activities.JoinNode;
+import fuml.syntax.activities.MergeNode;
+import fuml.syntax.classification.InstanceValue;
 import fuml.syntax.commonbehavior.OpaqueBehavior;
 import fuml.syntax.simpleclassifiers.PrimitiveType;
+import fuml.syntax.values.LiteralBoolean;
+import fuml.syntax.values.LiteralInteger;
+import fuml.syntax.values.LiteralNull;
+import fuml.syntax.values.LiteralReal;
+import fuml.syntax.values.LiteralString;
+import fuml.syntax.values.LiteralUnlimitedNatural;
 
-public abstract class ExecutionFactory extends org.modeldriven.fuml.FumlObject {
+public class ExecutionFactory extends org.modeldriven.fuml.FumlObject {
 
 	public fuml.semantics.loci.Locus locus = null;
 	public fuml.semantics.commonbehavior.OpaqueBehaviorExecutionList primitiveBehaviorPrototypes = new fuml.semantics.commonbehavior.OpaqueBehaviorExecutionList();
@@ -72,8 +174,229 @@ public abstract class ExecutionFactory extends org.modeldriven.fuml.FumlObject {
 
 	} // createEvaluation
 
-	public abstract fuml.semantics.loci.SemanticVisitor instantiateVisitor(
-			fuml.syntax.commonstructure.Element element);
+	public fuml.semantics.loci.SemanticVisitor instantiateVisitor(
+			fuml.syntax.commonstructure.Element element) {
+// Instantiate a visitor object for the given element.
+
+SemanticVisitor visitor = null;
+
+// Formerly Level L1
+
+if (element instanceof LiteralBoolean) {
+	visitor = new LiteralBooleanEvaluation();
+}
+
+else if (element instanceof LiteralString) {
+	visitor = new LiteralStringEvaluation();
+}
+
+else if (element instanceof LiteralNull) {
+	visitor = new LiteralNullEvaluation();
+}
+
+else if (element instanceof InstanceValue) {
+	visitor = new InstanceValueEvaluation();
+}
+
+else if (element instanceof LiteralUnlimitedNatural) {
+	visitor = new LiteralUnlimitedNaturalEvaluation();
+}
+
+else if (element instanceof LiteralInteger) {
+	visitor = new LiteralIntegerEvaluation();
+}
+
+else if (element instanceof LiteralReal) {
+	visitor = new LiteralRealEvaluation();
+}
+
+else if (element instanceof CallEventBehavior) {
+	visitor = new CallEventExecution();
+	
+// Formerly Level L2
+
+} else if (element instanceof Activity) {
+	visitor = new ActivityExecution();
+}
+
+else if (element instanceof ActivityParameterNode) {
+	visitor = new ActivityParameterNodeActivation();
+}
+
+else if (element instanceof CentralBufferNode) {
+	visitor = new CentralBufferNodeActivation();
+}
+
+else if (element instanceof InitialNode) {
+	visitor = new InitialNodeActivation();
+}
+
+else if (element instanceof ActivityFinalNode) {
+	visitor = new ActivityFinalNodeActivation();
+}
+
+else if (element instanceof FlowFinalNode) {
+	visitor = new FlowFinalNodeActivation();
+}
+
+else if (element instanceof JoinNode) {
+	visitor = new JoinNodeActivation();
+}
+
+else if (element instanceof MergeNode) {
+	visitor = new MergeNodeActivation();
+}
+
+else if (element instanceof ForkNode) {
+	visitor = new ForkNodeActivation();
+}
+
+else if (element instanceof DecisionNode) {
+	visitor = new DecisionNodeActivation();
+}
+
+else if (element instanceof InputPin) {
+	visitor = new InputPinActivation();
+}
+
+else if (element instanceof OutputPin) {
+	visitor = new OutputPinActivation();
+}
+
+else if (element instanceof CallBehaviorAction) {
+	visitor = new CallBehaviorActionActivation();
+}
+
+else if (element instanceof CallOperationAction) {
+	visitor = new CallOperationActionActivation();
+}
+
+else if (element instanceof SendSignalAction) {
+	visitor = new SendSignalActionActivation();
+}
+
+else if (element instanceof ReadSelfAction) {
+	visitor = new ReadSelfActionActivation();
+}
+
+else if (element instanceof TestIdentityAction) {
+	visitor = new TestIdentityActionActivation();
+}
+
+else if (element instanceof ValueSpecificationAction) {
+	visitor = new ValueSpecificationActionActivation();
+}
+
+else if (element instanceof CreateObjectAction) {
+	visitor = new CreateObjectActionActivation();
+}
+
+else if (element instanceof DestroyObjectAction) {
+	visitor = new DestroyObjectActionActivation();
+}
+
+else if (element instanceof ReadStructuralFeatureAction) {
+	visitor = new ReadStructuralFeatureActionActivation();
+}
+
+else if (element instanceof ClearStructuralFeatureAction) {
+	visitor = new ClearStructuralFeatureActionActivation();
+}
+
+else if (element instanceof AddStructuralFeatureValueAction) {
+	visitor = new AddStructuralFeatureValueActionActivation();
+}
+
+else if (element instanceof RemoveStructuralFeatureValueAction) {
+	visitor = new RemoveStructuralFeatureValueActionActivation();
+}
+
+else if (element instanceof ReadLinkAction) {
+	visitor = new ReadLinkActionActivation();
+}
+
+else if (element instanceof ClearAssociationAction) {
+	visitor = new ClearAssociationActionActivation();
+}
+
+else if (element instanceof CreateLinkAction) {
+	visitor = new CreateLinkActionActivation();
+}
+
+else if (element instanceof DestroyLinkAction) {
+	visitor = new DestroyLinkActionActivation();
+}
+
+// Formerly Level L3
+
+else if (element instanceof DataStoreNode) {
+	visitor = new DataStoreNodeActivation();
+} 
+
+else if (element instanceof ConditionalNode) {
+	visitor = new ConditionalNodeActivation();
+}
+
+else if (element instanceof LoopNode) {
+	visitor = new LoopNodeActivation();
+}
+
+else if (element instanceof ExpansionRegion) {
+	visitor = new ExpansionRegionActivation();
+}
+
+// Note: Since ConditionalNode, LoopNode and ExpansionRegion are
+// subclasses of StructuredActivityNode, element must be tested 
+// against the three subclasses before the superclass.
+else if (element instanceof StructuredActivityNode) {
+	visitor = new StructuredActivityNodeActivation();
+}
+
+else if (element instanceof ExpansionNode) {
+	visitor = new ExpansionNodeActivation();
+}
+
+else if (element instanceof ReadExtentAction) {
+	visitor = new ReadExtentActionActivation();
+}
+
+else if (element instanceof ReadIsClassifiedObjectAction) {
+	visitor = new ReadIsClassifiedObjectActionActivation();
+}
+
+else if (element instanceof ReclassifyObjectAction) {
+	visitor = new ReclassifyObjectActionActivation();
+}
+
+else if (element instanceof StartObjectBehaviorAction) {
+	visitor = new StartObjectBehaviorActionActivation();
+}
+
+else if (element instanceof StartClassifierBehaviorAction) {
+	visitor = new StartClassifierBehaviorActionActivation();
+}
+
+// Note: Since AcceptCallAction is a subclass of AcceptEventAction,
+// element must be tested against AcceptCallAction before
+// AcceptEventAction.
+else if (element instanceof AcceptCallAction) {
+	visitor = new AcceptCallActionActivation();
+}
+
+else if (element instanceof AcceptEventAction) {
+	visitor = new AcceptEventActionActivation();
+}
+
+else if (element instanceof ReplyAction) {
+	visitor = new ReplyActionActivation();
+}
+
+else if (element instanceof ReduceAction) {
+	visitor = new ReduceActionActivation();
+}
+
+		return visitor;
+	}
 
 	public fuml.semantics.commonbehavior.OpaqueBehaviorExecution instantiateOpaqueBehaviorExecution(
 			fuml.syntax.commonbehavior.OpaqueBehavior behavior) {
--- org.modeldriven.fuml/src/main/java/fuml/semantics/loci/ExecutionFactoryL3.java
+++ org.modeldriven.fuml/src/main/java/fuml/semantics/loci/ExecutionFactoryL3.java
@@ -2,7 +2,7 @@
  * Initial version copyright 2008 Lockheed Martin Corporation, except  
  * as stated in the file entitled Licensing-Information. 
  * 
- * All modifications copyright 2009-2017 Data Access Technologies, Inc.
+ * All modifications copyright 2009-2018 Data Access Technologies, Inc.
  *
  * Licensed under the Academic Free License version 3.0 
  * (http://www.opensource.org/licenses/afl-3.0.php), except as stated 
@@ -11,119 +11,7 @@
 
 package fuml.semantics.loci;
 
-import fuml.semantics.actions.AcceptCallActionActivation;
-import fuml.semantics.actions.AcceptEventActionActivation;
-import fuml.semantics.actions.ConditionalNodeActivation;
-import fuml.semantics.actions.ExpansionNodeActivation;
-import fuml.semantics.actions.ExpansionRegionActivation;
-import fuml.semantics.actions.LoopNodeActivation;
-import fuml.semantics.actions.ReadExtentActionActivation;
-import fuml.semantics.actions.ReadIsClassifiedObjectActionActivation;
-import fuml.semantics.actions.ReclassifyObjectActionActivation;
-import fuml.semantics.actions.ReduceActionActivation;
-import fuml.semantics.actions.ReplyActionActivation;
-import fuml.semantics.actions.StartClassifierBehaviorActionActivation;
-import fuml.semantics.actions.StartObjectBehaviorActionActivation;
-import fuml.semantics.actions.StructuredActivityNodeActivation;
-import fuml.semantics.activities.DataStoreNodeActivation;
-import fuml.syntax.actions.AcceptCallAction;
-import fuml.syntax.actions.AcceptEventAction;
-import fuml.syntax.actions.ConditionalNode;
-import fuml.syntax.actions.ExpansionNode;
-import fuml.syntax.actions.ExpansionRegion;
-import fuml.syntax.actions.LoopNode;
-import fuml.syntax.actions.ReadExtentAction;
-import fuml.syntax.actions.ReadIsClassifiedObjectAction;
-import fuml.syntax.actions.ReclassifyObjectAction;
-import fuml.syntax.actions.ReduceAction;
-import fuml.syntax.actions.ReplyAction;
-import fuml.syntax.actions.StartClassifierBehaviorAction;
-import fuml.syntax.actions.StartObjectBehaviorAction;
-import fuml.syntax.actions.StructuredActivityNode;
-import fuml.syntax.activities.DataStoreNode;
-
 public class ExecutionFactoryL3 extends
 		fuml.semantics.loci.ExecutionFactoryL2 {
-
-	public fuml.semantics.loci.SemanticVisitor instantiateVisitor(
-			fuml.syntax.commonstructure.Element element) {
-		// Instantiate a visitor object for the given element (at Conformance
-		// Level 3)
-
-		SemanticVisitor visitor = null;
-		
-		if (element instanceof DataStoreNode) {
-			visitor = new DataStoreNodeActivation();
-		} 
-		
-		else if (element instanceof ConditionalNode) {
-			visitor = new ConditionalNodeActivation();
-		}
-
-		else if (element instanceof LoopNode) {
-			visitor = new LoopNodeActivation();
-		}
-
-		else if (element instanceof ExpansionRegion) {
-			visitor = new ExpansionRegionActivation();
-		}
-
-		// Note: Since ConditionalNode, LoopNode and ExpansionRegion are
-		// subclasses of StructuredActivityNode, element must be tested 
-		// against the three subclasses before the superclass.
-		else if (element instanceof StructuredActivityNode) {
-			visitor = new StructuredActivityNodeActivation();
-		}
-
-		else if (element instanceof ExpansionNode) {
-			visitor = new ExpansionNodeActivation();
-		}
-
-		else if (element instanceof ReadExtentAction) {
-			visitor = new ReadExtentActionActivation();
-		}
-
-		else if (element instanceof ReadIsClassifiedObjectAction) {
-			visitor = new ReadIsClassifiedObjectActionActivation();
-		}
-
-		else if (element instanceof ReclassifyObjectAction) {
-			visitor = new ReclassifyObjectActionActivation();
-		}
-
-		else if (element instanceof StartObjectBehaviorAction) {
-			visitor = new StartObjectBehaviorActionActivation();
-		}
-
-		else if (element instanceof StartClassifierBehaviorAction) {
-			visitor = new StartClassifierBehaviorActionActivation();
-		}
-
-		// Note: Since AcceptCallAction is a subclass of AcceptEventAction,
-		// element must be tested against AcceptCallAction before
-		// AcceptEventAction.
-		else if (element instanceof AcceptCallAction) {
-			visitor = new AcceptCallActionActivation();
-		}
-
-		else if (element instanceof AcceptEventAction) {
-			visitor = new AcceptEventActionActivation();
-		}
-
-		else if (element instanceof ReplyAction) {
-			visitor = new ReplyActionActivation();
-		}
-
-		else if (element instanceof ReduceAction) {
-			visitor = new ReduceActionActivation();
-		}
-
-		else {
-			visitor = super.instantiateVisitor(element);
-		}
-
-		return visitor;
-
-	} // instantiateVisitor
 
 } // ExecutionFactoryL3
